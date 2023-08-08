14fe34b34d96d529f9425d7965c9bd7fedf9b3fe
John Erick Santos <johnsantosdev@gmail.com>
2023-08-09 05:37:29 +1000

    feat: Home Table view setup

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -8,9 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let timelineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(timelineTableView)
+        timelineTableView.delegate = self
+        timelineTableView.dataSource = self
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        timelineTableView.frame = view.frame
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Tweet"
+
+        return cell
     }
 }
