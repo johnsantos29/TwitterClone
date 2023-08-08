2095ed7ec0a512bdcc88716f48edae24c65f7162
John Erick Santos <johnsantosdev@gmail.com>
2023-08-09 05:10:19 +1000

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by John Erick Santos on 9/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
