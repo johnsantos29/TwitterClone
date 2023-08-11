3d7965ab5a8a5f548672ed7904fa66505a112a7e
John Erick Santos <johnsantosdev@gmail.com>
2023-08-12 08:00:51 +1000

    feat: Profile Header View

--- TwitterClone/Controllers/General/ProfileViewController.swift
+++ TwitterClone/Controllers/General/ProfileViewController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    private let profileTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         navigationItem.title = "Profile"
+        view.addSubview(profileTableView)
+
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+
+        let profileTableHeaderView = ProfileTableHeaderView(frame: CGRect(x: 0, y: 0, width: profileTableView.frame.width, height: 380))
+        profileTableView.tableHeaderView = profileTableHeaderView
+
+        configureConstraints()
+    }
+
+    // MARK: - Constraints
+
+    private func configureConstraints() {
+        let profileTableViewConstraints = [
+            profileTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            profileTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ]
+
+        NSLayoutConstraint.activate(profileTableViewConstraints)
+    }
+}
+
+// MARK: - Table View Delegate / DataSource
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else {
+            return UITableViewCell()
+        }
+
+        return cell
     }
 }
